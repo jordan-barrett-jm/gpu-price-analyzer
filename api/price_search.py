bfff676fb2fde6bc201b98071cbe65c5f8a3b118
jordan-barrett-jm <jordan.barrett.jm@gmail.com>
2021-07-25 16:31:21 -0500

    implemented while loop to ensure a minimum number of valid posts are captured

--- api/price_search.py
+++ api/price_search.py
@@ -12,18 +12,22 @@ params = {
     "size": limit,
     "title": gpu
 }
-
-req_data = requests.get("https://api.pushshift.io/reddit/search/submission/", params=params)
 price_data = []
 
-for post in req_data.json()["data"]:
-    if "link_flair_text" in post:
-        if post["link_flair_text"] == "SELLING":
-            post_data = {
-                'author': post['author'],
-                'post_title': post['title'],
-                'post_body': post['selftext'],
-                'link': post['full_link']
-            }
-            price_data.append(post_data)
-            pp(post_data)
+while len(price_data) < 5:
+    req_data = requests.get("https://api.pushshift.io/reddit/search/submission/", params=params)
+
+
+    for post in req_data.json()["data"]:
+        if "link_flair_text" in post:
+            if post["link_flair_text"] == "SELLING":
+                post_data = {
+                    'author': post['author'],
+                    'post_title': post['title'],
+                    'post_body': post['selftext'],
+                    'link': post['full_link']
+                }
+                price_data.append(post_data)
+                
+
+pp(price_data)
