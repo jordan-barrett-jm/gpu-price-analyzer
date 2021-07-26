9f76410ff495a6e1ea05807b978b3d30daaace8d
jordan-barrett-jm <jordan.barrett.jm@gmail.com>
2021-07-26 17:29:37 -0500

    added form and table to view GPU price info

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
     constructor(props){
         super(props);
         this.state = {
             gpu_name: '',
-            price_data: ''
+            price_data: []
         }
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -16,16 +17,47 @@ class App extends React.Component {
         });
     }
     
-    handleSubmit(event){
+    async handleSubmit(event){
         console.log(this.state.gpu_name);
+        event.preventDefault();
+        await axios.post("http://127.0.0.1:9000/api", 
+        {"gpu_name": this.state.gpu_name}).then(res=> {
+            console.log(res.data);
+            this.setState({
+                price_data: res.data
+            });
+        })
     }
     render(){
         return (
+            <div>
             <form onSubmit={this.handleSubmit}>
                 <label>GPU Name</label>
-                <input type="text" value={this.state.price_data} onChange={this.handleChange}/>
+                <input type="text" value={this.state.gpu_name} onChange={this.handleChange}/>
                 <input type="submit" value="Submit"/>
             </form>
+            {this.state.price_data.length > 0 ? 
+                <div>
+                    <table>
+                        <thead>
+                            <th>Post Title</th>
+                            <th>Post Body</th>
+                            <th>Post Link</th>
+                        </thead>
+                        <tbody>
+                        {this.state.price_data.map((post) => (
+                            <tr>
+                                <td>{post.post_title}</td>
+                                <td>{post.post_body}</td>
+                                <td>{post.link}</td>
+                            </tr>
+                        ))}
+                        </tbody>
+                        
+                    </table>
+                </div>
+             : "ntn yet" }
+            </div>
         );
   }
 }
