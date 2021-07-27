103e345faef7a92bf4f1f3ce9c1c59f4892b5d57
jordan-barrett-jm <jordan.barrett.jm@gmail.com>
2021-07-27 11:48:38 -0500

    added price highlighting for easier selection by user

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,16 +6,37 @@ class App extends React.Component {
         super(props);
         this.state = {
             gpu_name: '',
-            price_data: []
+            price_data: [],
+            price_list: []
         }
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleClick = this.handleClick.bind(this);
     }
     handleChange(event){
         this.setState({
            gpu_name: event.target.value 
         });
     }
+    
+    handleClick(price){
+        console.log(price);
+        var arr = this.state.price_list;
+        arr.push(price);
+        this.setState({
+            price_list: arr
+        });
+    }
+    
+    renderParagraph(text){
+        var reg =  new RegExp('\\$[-0-9.,]+[-0-9.,a-zA-Z]*\\b');
+        var arr = text.split(" ").map((word) => {
+        return reg.test(word) === true ? <span onClick={() => this.handleClick(word)}><mark>{word}</mark> </span>
+        : <span>{word} </span>
+        });
+        return arr;
+    }
+
     
     async handleSubmit(event){
         console.log(this.state.gpu_name);
@@ -32,7 +53,7 @@ class App extends React.Component {
         return (
             <div>
             <form onSubmit={this.handleSubmit}>
-                <label>GPU Name</label>
+                <label>GPU Name: </label>
                 <input type="text" value={this.state.gpu_name} onChange={this.handleChange}/>
                 <input type="submit" value="Submit"/>
             </form>
@@ -48,7 +69,7 @@ class App extends React.Component {
                         {this.state.price_data.map((post) => (
                             <tr>
                                 <td>{post.post_title}</td>
-                                <td>{post.post_body}</td>
+                                <td>{this.renderParagraph(post.post_body)}</td>
                                 <td>{post.link}</td>
                             </tr>
                         ))}
@@ -56,7 +77,18 @@ class App extends React.Component {
                         
                     </table>
                 </div>
-             : "ntn yet" }
+             : <div>Price data not found</div> }
+             
+             {this.state.price_list.length > 0 ? 
+                <div>
+                    <ul>
+                    {this.state.price_list.map( price => (
+                        <li>{price}</li>
+                    ) )}
+                    </ul>
+                </div>
+             
+             : <div>Price list empty</div>}
             </div>
         );
   }
