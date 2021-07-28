d1364cb730d556d64d0a657b47a2bb2c936f95d0
jordan-barrett-jm <jordan.barrett.jm@gmail.com>
2021-07-28 17:38:12 -0500

    the python price searcher now only returns posts where the prices are identified and there is only one item on sale

--- analyzer_api/price_analyzer/price_search.py
+++ analyzer_api/price_analyzer/price_search.py
@@ -5,31 +5,38 @@ import requests
 import json
 from pprint import pp
 import datetime
+import re
 
 
 def priceSearch(gpu_name):
     price_data = []
     last = int(datetime.datetime.now().timestamp())
+    r = r'\$[-0-9.,]+[-0-9.,a-zA-Z]*\b'
     while len(price_data) < 5:
         params = {
-            "size": 10,
+            "size": 50,
             "title": gpu_name,
             'before': last
         }
         req_data = requests.get("https://api.pushshift.io/reddit/search/submission/", params=params).json()
 
-
+        
         for post in req_data["data"]:
-            if "link_flair_text" in post:
-                print (post)
-                if post["link_flair_text"] == "SELLING" and "[removed]" not in post["selftext"]:
-                    post_data = {
-                        'author': post['author'],
-                        'post_title': post['title'],
-                        'post_body': post['selftext'],
-                        'link': post['full_link']
-                    }
-                    price_data.append(post_data)
+            if len(price_data) < 5:
+                if "link_flair_text" in post:
+                    print (post)
+                    if post["link_flair_text"] == "SELLING" and "[removed]" not in post["selftext"]:
+                        match = re.findall(r, post['selftext'])
+                        if len(match) == 1:
+                            post_data = {
+                                'author': post['author'],
+                                'post_title': post['title'],
+                                'post_body': post['selftext'],
+                                'link': post['full_link']
+                            }
+                            price_data.append(post_data)
+            else:
+                break
         last = req_data["data"][-1]["created_utc"]           
 
     return price_data
